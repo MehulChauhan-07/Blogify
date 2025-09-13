6a782ec2567e7a508219a869c3b67a497a8fa002
MehulChauhan-07 <mehulsinhchauhan5124@gmail.com>
2025-09-13 22:11:10 +0530

    Update Firebase config handling and .env example
    Expanded .env.example with detailed Firebase config variables and updated firebase.js to use getEvn for environment variable access. This improves configuration clarity and consistency across environments.

--- client/src/helpers/firebase.js
+++ client/src/helpers/firebase.js
@@ -8,13 +8,13 @@ import { getEvn } from "./getEnv";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: getEvn("VITE_FIREBASE_API_KEY"),
+  authDomain: getEvn("VITE_FIREBASE_AUTH_DOMAIN"),
+  projectId: getEvn("VITE_FIREBASE_PROJECT_ID"),
+  storageBucket: getEvn("VITE_FIREBASE_STORAGE_BUCKET"),
+  messagingSenderId: getEvn("VITE_FIREBASE_MESSAGING_SENDER_ID"),
+  appId: getEvn("VITE_FIREBASE_APP_ID"),
+  measurementId: getEvn("VITE_FIREBASE_MEASUREMENT_ID"),
 };
 
 // Initialize Firebase
